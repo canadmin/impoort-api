467406fb2847c25a56c388f78b1eeb8a5ed123ee
canadmin <icanyard@gmail.com>
2019-09-30 23:04:28 +0300

    created user model

---
+++ src/main/java/com/impoort/impoortapi/domain/User.java
@@ -0,0 +1,26 @@
+package com.impoort.impoortapi.domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+
+    private String userId;
+    private String ActiveGuide;
+    private String description;
+    private boolean isActive;
+    private boolean isConfirmed;
+    private String sector;
+    private int userType;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String city;
+    private Date birthDate;
+    private String gender;
+    private String phoneNumber;
+    private String experienceYear;
+    private List<String> experienceCompanies;
+    private int employeeCount;
+}
