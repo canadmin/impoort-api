365008ff176c605d34fe8d0300eca37bb9764888
canadmin <icanyard@gmail.com>
2019-09-30 22:57:50 +0300

    initial commit

---
+++ src/main/java/com/impoort/impoortapi/ImpoortApiApplication.java
@@ -0,0 +1,13 @@
+package com.impoort.impoortapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpoortApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpoortApiApplication.class, args);
+    }
+
+}
