04d05d42f3572a0f51c8089e600d580d1da66447
hasancerit <hasan__cerit@hotmail.com>
2019-09-30 23:24:59 +0300

    created post model

---
+++ src/main/java/com/impoort/impoortapi/domain/post/Post.java
@@ -0,0 +1,11 @@
+package com.impoort.impoortapi.domain;
+
+public class Post {
+    private String postId;
+    private User user;
+    private int postType;
+    private String mediaUrl;
+    private String postDesctription;
+    
+
+}
