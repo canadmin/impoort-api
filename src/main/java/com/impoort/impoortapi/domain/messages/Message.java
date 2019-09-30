49f1262f301973023a8bd6252fb375b9ae48f993
yusufalicezik <yusufalicezik35@gmail.com>
2019-09-30 23:12:48 +0300

    Messages classes added

---
+++ src/main/java/com/impoort/impoortapi/domain/messages/Message.java
@@ -0,0 +1,14 @@
+package com.impoort.impoortapi.domain.messages;
+
+import java.util.Date;
+
+public class Message {
+
+    private int messageId;
+    private String messagaSenderUserId;
+    private String messagaReceiverUserId;
+    private String messageText;
+    private Date messageDate;
+
+
+}
---
+++ src/main/java/com/impoort/impoortapi/domain/messages/MessagesGeneral.java
@@ -0,0 +1,6 @@
+package com.impoort.impoortapi.domain.messages;
+
+public class MessagesGeneral {
+    private String userId;
+    private String userMessagesWithID;
+}
