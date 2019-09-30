e26578904bd24d94842a11215be267b61f924fcb
canadmin <icanyard@gmail.com>
2019-09-30 23:14:01 +0300

    fix veriable name

--- src/main/java/com/impoort/impoortapi/domain/messages/Message.java
+++ src/main/java/com/impoort/impoortapi/domain/messages/Message.java
@@ -5,8 +5,8 @@ import java.util.Date;
 public class Message {
 
     private int messageId;
-    private String messagaSenderUserId;
-    private String messagaReceiverUserId;
+    private String messageSenderUserId;
+    private String messageReceiverUserId;
     private String messageText;
     private Date messageDate;
 
