62e33479072e752f3132c67ae3db58d7253aec24
yusufalicezik <yusufalicezik35@gmail.com>
2019-10-01 14:20:26 +0300

    Mail sender added and tested in UserController

--- src/main/java/com/impoort/impoortapi/controller/UserController.java
+++ src/main/java/com/impoort/impoortapi/controller/UserController.java
@@ -4,17 +4,18 @@ import com.impoort.impoortapi.api.v1.model.requestmodel.UserRequestDTO;
 import com.impoort.impoortapi.api.v1.model.responsemodel.UserResponseDTO;
 import com.impoort.impoortapi.domain.user.User;
 import com.impoort.impoortapi.service.UserService;
+import com.impoort.impoortapi.utils.MailSenderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 @Controller
@@ -23,6 +24,9 @@ public class UserController {
     private final UserService userService;
     private final ModelMapper modelMapper;
 
+    @Autowired
+    public JavaMailSender emailSender;
+
     public UserController(UserService userService, ModelMapper modelMapper) {
         this.userService = userService;
         this.modelMapper = modelMapper;
@@ -30,6 +34,9 @@ public class UserController {
 
     @GetMapping("/users")
     public ResponseEntity<List<UserResponseDTO>>  getAllUser(){
+        /* Mail Sender Test
+        MailSenderService mailSenderService = new MailSenderService(emailSender);
+        mailSenderService.sendSimpleMessage("yusufali.cezik@hotmail.com", "", false, "Yusuf Ali Çezik");*/
           return new ResponseEntity<List<UserResponseDTO>>(userService.getAllUser(), HttpStatus.OK);
     }
     @PostMapping("/addUser")
---
+++ src/main/java/com/impoort/impoortapi/utils/MailSenderService.java
@@ -0,0 +1,25 @@
+package com.impoort.impoortapi.utils;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class MailSenderService {
+    private JavaMailSender emailSender;
+
+    public MailSenderService(JavaMailSender emailSender){
+        this.emailSender = emailSender;
+    }
+
+    public void sendSimpleMessage(String mail, String text, Boolean isActive, String name) {
+        if (!isActive) {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(mail);
+            message.setSubject("Hesap Onayı");
+            message.setText("Merhaba " + name.toUpperCase() + " , \n " + "Lütfen hesabını onaylamak aşağıdaki linke tıkla \n \n " +
+                    "localhost:8081/api/users/verifyAcount/url/" + text);
+            emailSender.send(message);
+        }
+
+    }
+
+}
