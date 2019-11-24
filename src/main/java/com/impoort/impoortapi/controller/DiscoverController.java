df9a9016347909da552c1daaf9b0c237f181d63c
canadmin <icanyard@gmail.com>
2019-11-24 07:48:34 +0300

    added discover Post. Closes #24 .

---
+++ src/main/java/com/impoort/impoortapi/controller/DiscoverController.java
@@ -0,0 +1,30 @@
+package com.impoort.impoortapi.controller;
+
+import com.impoort.impoortapi.api.v1.model.responsemodel.PostResponseDTO;
+import com.impoort.impoortapi.domain.post.Post;
+import com.impoort.impoortapi.service.DiscoverService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/discover")
+public class DiscoverController {
+
+    private final DiscoverService discoverService;
+
+    public DiscoverController(DiscoverService discoverService) {
+        this.discoverService = discoverService;
+    }
+
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntity<List<PostResponseDTO>> discoverPosts(){
+        return new ResponseEntity<>(discoverService.discoverPosts(), HttpStatus.OK);
+    }
+}
